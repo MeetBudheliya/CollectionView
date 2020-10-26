4c1a352923ebebf89f5540494492fde67cd5b368
MeetBudheliya <meetbudheliya04@gmail.com>
2020-10-26 11:35:31 +0530

    commit

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -13,27 +13,57 @@ struct Data
 }
 class ViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
     
-    var dataList = Data()
+    var dataList = [Data()]
     @IBOutlet weak var collection1: UICollectionView!
     @IBOutlet weak var collection2: UICollectionView!
     @IBOutlet weak var collection3: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataList = Data(image: [#imageLiteral(resourceName: "Skype"),#imageLiteral(resourceName: "SnapChat"),#imageLiteral(resourceName: "Shopify"),#imageLiteral(resourceName: "Calculator"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "DropBox")], name: ["Skype","SnapChat","Shopify","Calculator","DropBox","Skype","SnapChat","Shopify","Calculator","DropBox"])
+        dataList = [Data(image: [#imageLiteral(resourceName: "Skype"),#imageLiteral(resourceName: "SnapChat"),#imageLiteral(resourceName: "Shopify"),#imageLiteral(resourceName: "Calculator"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "YouTube"),#imageLiteral(resourceName: "Messeges"),#imageLiteral(resourceName: "Chrome"),#imageLiteral(resourceName: "iTunes"),#imageLiteral(resourceName: "ApppStore")], name:["Skype","SnapChat","Shopify","Calculator","DropBox","Youtube","Messages","Chrome","iTunes","Appstore"]),
+                    Data(image: [#imageLiteral(resourceName: "Chrome"),#imageLiteral(resourceName: "Shopify"),#imageLiteral(resourceName: "Skype"),#imageLiteral(resourceName: "Calculator"),#imageLiteral(resourceName: "Messeges"),#imageLiteral(resourceName: "iTunes")], name: ["Chrome","Shopify","Skype","Calculator","Messages","iTunes"]),
+                    Data(image: [#imageLiteral(resourceName: "ApppStore"),#imageLiteral(resourceName: "FakeCall"),#imageLiteral(resourceName: "DropBox"),#imageLiteral(resourceName: "SnapChat"),#imageLiteral(resourceName: "YouTube"),#imageLiteral(resourceName: "Battlenet"),#imageLiteral(resourceName: "Skype")], name: ["Appstore","Call","Dropbox","Snapchat","Youtube","Spycraft","Skype"])]
         
-       
-       collection1.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        collection1.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        collection2.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        collection3.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
     }
-   
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        if collectionView == collection1
+        {
+            return dataList[0].image.count
+        }
+        else if collectionView == collection2
+        {
+            return dataList[1].image.count
+        }
+        else if collectionView == collection3
+        {
+            return dataList[2].image.count
+        }
+        
+        return 1
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collection1.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        cell.cellImage.image = dataList.image[indexPath.row]
-        cell.cellLabel.text = dataList.name[indexPath.row]
+        if collectionView == collection1
+        {
+            cell.cellImage.image = dataList[0].image[indexPath.row]
+            cell.cellLabel.text = dataList[0].name[indexPath.row]
+            
+        }
+        else if collectionView == collection2
+        {
+            cell.cellImage.image = dataList[1].image[indexPath.row]
+            cell.cellLabel.text = dataList[1].name[indexPath.row]
+        }
+        else if collectionView == collection3
+        {
+            cell.cellImage.image = dataList[2].image[indexPath.row]
+            cell.cellLabel.text = dataList[2].name[indexPath.row]
+        }
         return cell
     }
 }
